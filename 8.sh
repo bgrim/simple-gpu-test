9e5017f9de45711b4cee6f4cc205b2e3d7719c00
bgrim <ben.grimmer@ossm.edu>
2012-06-21 11:31:27 -0500

    added tests for launching 8 applications at once

---
+++ 8.sh
@@ -0,0 +1,27 @@
+#! /bin/bash                                                                                                                                                                       
+nkernels=1
+# 1 through 5 for the throttles
+# for i in {1..5}
+for i in {1..5}
+do
+sleeptime=1000
+    # 1 through 8 for the sleeptimes
+    # for k in {1..8}
+    for k in {1..5}
+    do
+	echo ""
+	echo ""
+	echo ""
+	echo "Throttle count equals: " $throttle    
+	echo "Sleeptime is equal to: " $sleeptime
+	echo "The number of kernels: " $nkernels
+	
+	# run swift
+	time swift -tc.file tc.data 8.swift -nkernels=$nkernels -kernel_time=$sleeptime
+	
+        # double the sleeptime
+	sleeptime=$(expr $sleeptime + $sleeptime)
+    done
+# increase the nkernels
+nkernels=$(expr $nkernels + $nkernels)
+done
